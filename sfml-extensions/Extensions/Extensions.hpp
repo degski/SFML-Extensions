3e674f28d44e50cae25285a8030e3ad1c6730817
degski <degski@gmail.com>
2019-02-08 11:47:46 +0200

    cleanup, reformat

--- sfml-extensions/Extensions/Extensions.hpp
+++ sfml-extensions/Extensions/Extensions.hpp
@@ -66,7 +66,7 @@
 
 
 
-using namespace std::placeholders;  // for _1, _2, _3...
+using namespace std::placeholders;  // for _1, _2, _3.
 
 
 
@@ -79,9 +79,9 @@ using HrTimePoint = HrClock::time_point;
 
 enum class ScreenSizeType : Int32 { Small = 576, Medium = 614, Large = 864 };
 
-// Small  : 1280 x 720                  -> height 576...
-// Medium : 1024 x 768 (or 1366 � 768 ) -> height 614...
-// Large  : 1920 x 1080                 -> height 864...
+// Small  : 1280 x 720                  -> height 576.
+// Medium : 1024 x 768 (or 1366 x 768 ) -> height 614.
+// Large  : 1920 x 1080                 -> height 864.
 
 
 using Point = Vector2f;
@@ -120,13 +120,12 @@ using are_valid_types = std::conjunction<is_real<R>, is_unsigned_integral<I>>;
 
 template<typename RType, typename Type = RType>
 struct Factory {
-
     template<typename ... Args>
     RType * operator ( ) ( Args && ... args ) { return new Type ( std::forward<Args> ( args )... ); }
 };
 
 
-// Dealing with radians...
+// Dealing with radians.
 
 constexpr float pi = 3.1415926535897932384626433832795;
 constexpr float half_pi = 1.5707963267948966192313216916398;
@@ -135,35 +134,30 @@ constexpr float two_pi = 6.283185307179586476925286766559;
 
 template<typename real, typename sfinae = typename std::enable_if<detail::is_real<real>::value>::type>
 real radianToDegree ( const real radian_ ) noexcept {
-
     return real { 180 } / pi * radian_;
 }
 
-
 template<typename real, typename sfinae = typename std::enable_if<detail::is_real<real>::value>::type>
 real degreeToRadian ( const real degree_ ) noexcept {
-
     return pi / real { 180 } * degree_;
 }
 
-// Between 0 and 2 pi...
+// Between 0 and 2 pi.
 float clampRadians ( float r_ ) noexcept;
 // The second parameter is a boolean value, true for round-up,
-//  false for round-down...
+//  false for round-down.
 float makeOdd ( const float v_, const bool round_up_ = true ) noexcept;
-// Sleep for a number of milliseconds...
+// Sleep for a number of milliseconds.
 void sleepForMilliseconds ( const Int32 milliseconds_ ) noexcept;
 void sleepForMicroseconds ( const Int32 microseconds_ ) noexcept;
 
 std::string systemTime ( ) noexcept;
 
 inline void timeBeginPeriod ( ) noexcept {
-
     ::timeBeginPeriod ( 1 );
 }
 
 inline void timeEndPeriod ( ) noexcept {
-
     ::timeEndPeriod ( 1 );
 }
 
@@ -178,7 +172,6 @@ Int32 getScreenRefreshRate ( ) noexcept;
 ScreenSizeType getScreenSizeType ( ) noexcept;
 
 struct SquareShape : RectangleShape {
-
     SquareShape ( ) : RectangleShape ( ) { }
     SquareShape ( const float s_ ) : RectangleShape ( Vector2f ( s_, s_ ) ) { }
 };
@@ -192,111 +185,86 @@ bool segmentIntersectsRectangle ( const Point & p1_, const Point & p2_, const Re
 std::optional<Point> lineSegmentIntersection ( const Point & p0_, const Point & p1_, const Point & p2_, const Point & p3_ ) noexcept;
 std::optional<Point> lineSegmentIntersectionStrict ( const Point & p0_, const Point & p1_, const Point & p2_, const Point & p3_ ) noexcept;
 
-// If ( rv > 0 ) then C is to the left...
-// If ( rv = 0 ) then C is on the same line...
-// If ( rv < 0 ) then C is to the right...
+// If ( rv > 0 ) then C is to the left.
+// If ( rv = 0 ) then C is on the same line.
+// If ( rv < 0 ) then C is to the right.
+
+// 2D Cross Product.
 template<typename real, typename sfinae = typename std::enable_if<detail::is_real<real>::value>::type>
 real perpDotProduct ( const Vector2<real> & a_, const Vector2<real> & b_ ) noexcept {
-
-    // 2D Cross Product...
-
     return a_.x * b_.y - a_.y * b_.x;
 }
 
-// If ( rv > 0 ) then C is to the left...
-// If ( rv = 0 ) then C is on the same line...
-// If ( rv < 0 ) then C is to the right...
+// If ( rv > 0 ) then C is to the left.
+// If ( rv = 0 ) then C is on the same line.
+// If ( rv < 0 ) then C is to the right.
+
+// 2D Cross Product.
 template<typename real, typename sfinae = typename std::enable_if<detail::is_real<real>::value>::type>
 real perpDotProduct ( const Vector2<real> & a_, const Vector2<real> & b_, const Vector2<real> & c_ ) noexcept {
-
-    // 2D Cross Product...
-
     return perpDotProduct ( b_ - a_, c_ - a_ );
 }
 
 
 template<typename real, typename sfinae = typename std::enable_if<detail::is_real<real>::value>::type>
 real dotProduct ( const Vector2<real> & a_, const sf::Vector2<real> & b_ ) noexcept {
-
     return a_.x * b_.x + a_.y * b_.y;
 }
 
 template<typename real, typename sfinae = typename std::enable_if<detail::is_real<real>::value>::type>
 real squaredLength ( const Vector2<real> & v_ ) noexcept {
-
     return v_.x * v_.x + v_.y * v_.y;
 }
 
 template<typename real, typename sfinae = typename std::enable_if<detail::is_real<real>::value>::type>
 real length ( const Vector2<real> & v_ ) noexcept {
-
     return std::sqrt ( v_.x * v_.x + v_.y * v_.y );
 }
 
 
 template<typename real, typename sfinae = typename std::enable_if<detail::is_real<real>::value>::type>
 Vector2<real> normalize ( const Vector2<real> & v_ ) noexcept {
-
     const real length = length ( v_ );
-
     return { v_.x / length, v_.y / length };
 }
 
 
 template<typename S>
 S & operator << ( S & out_, const Vector2f & v_ ) {
-
     std::printf ( "<%+0.1f %+0.1f>", v_.x, v_.y );
-
     return out_;
 }
 
 template<typename S>
 S & operator << ( S & out_, const Vector2i & v_ ) {
-
     std::printf ( "<%+i %+i>", v_.x, v_.y );
-
     return out_;
 }
 
 
-// Loading resources...
+// Loading resources.
 
 // Loads T (Image, Texture, Font, SoundBuffer)
-// with image data from a resource (.rc) file...
+// with image data from a resource (.rc) file.
 template<typename T>
 void loadFromResource ( T &destination_, const Int32 name_ ) {
-
     HRSRC rsrc_data = FindResource ( NULL, MAKEINTRESOURCE ( name_ ), L"FILEDATA" );
-
     if ( ! ( rsrc_data ) ) {
-
         throw std::runtime_error ( "Failed to find resource." );
     }
-
     DWORD rsrc_data_size = SizeofResource ( NULL, rsrc_data );
-
     if ( rsrc_data_size <= 0 ) {
-
         throw std::runtime_error ( "Size of resource is 0." );
     }
-
     HGLOBAL grsrc_data = LoadResource ( NULL, rsrc_data );
-
     if ( ! ( grsrc_data ) ) {
-
         throw std::runtime_error ( "Failed to load resource." );
     }
-
     LPVOID first_byte = LockResource ( grsrc_data );
-
     if ( ! ( first_byte ) ) {
-
         throw std::runtime_error ( "Failed to lock resource." );
     }
-
     if ( ! ( destination_.loadFromMemory ( first_byte, rsrc_data_size ) ) ) {
-
         throw std::runtime_error ( "Failed to load resource from memory." );
     }
 }
@@ -308,80 +276,51 @@ void loadFromResource ( Shader &shader_, const Shader::Type type_, const Int32 n
 void loadFromResource ( Shader &shader_, const Int32 vertex_name_, const Int32 fragment_name_ );
 
 
-// Vector2 casting...
+// Vector2 casting.
 
 template<typename T>
 Vector2f castVector2f ( const T x_, const T y_ ) {
-
     return Vector2f ( static_cast< float > ( x_ ), static_cast< float > ( y_ ) );
 }
 
 template<typename T>
 Vector2f castVector2f ( const Vector2<T> &v_ ) {
-
     return castVector2f ( v_.x, v_.y );
 }
 
 template<typename T>
 Vector2i castVector2i ( const T x_, const T y_ ) {
-
     return Vector2i ( static_cast< Int32 > ( x_ ), static_cast< Int32 > ( y_ ) );
 }
 
 template<typename T>
 Vector2i castVector2i ( const Vector2<T> &v_ ) {
-
     return castVector2i ( v_.x, v_.y );
 }
 
 
-// Apply same sort to more than 1 vector...
+// Apply same sort to more than 1 vector.
 template<typename T>
 std::vector<std::size_t> sortPermutation ( const std::vector<T> &vector_ ) {
-
     std::vector<std::size_t> permutation ( vector_.size ( ) );
-
     std::iota ( permutation.begin ( ), permutation.end ( ), 0 );
     std::sort ( permutation.begin ( ), permutation.end ( ), [ & ] ( std::size_t i, std::size_t j ) { return vector_ [ i ] < vector_ [ j ]; } );
-
     return permutation;
 }
 
 template <typename T>
 std::vector<T> applyPermutation ( const std::vector<T> &vector_, const std::vector<std::size_t> &permutation_ ) {
-
     std::vector<T> sorted_vector ( permutation_.size ( ) );
-
     std::transform ( permutation_.begin ( ), permutation_.end ( ), sorted_vector.begin ( ), [ & ] ( std::size_t i ) { return vector_ [ i ]; } );
-
     return sorted_vector;
 }
 
 
 template <typename T>
 void negateAbs ( T &x_ ) noexcept {
-
     if ( x_ > T { 0 } ) {
-
         x_ = -x_;
     }
-}
-
-
-template <typename T>
-T clamp ( const T x_, const T a_, const T b_ ) noexcept {
-
-    if ( x_ >= b_ ) {
-
-        return b_;
-    }
-
-    if ( x_ <= a_ ) {
-
-        return a_;
-    }
-
-    return x_;
 }
 
 
@@ -403,7 +342,6 @@ struct Pacer {
 
     IntDuration m_duration;
     HrTimePoint m_start, m_time;
-
     unsigned int m_ui;
 
     int c_start;
@@ -411,19 +349,15 @@ struct Pacer {
     const Int64 m_frequency;
 
     Int64 qpfrequency ( ) const noexcept {
-
         timeBeginPeriod ( );
         LARGE_INTEGER f;
         QueryPerformanceFrequency ( &f );
-
         return ( Int64 ) std::ceil ( ( double ) f.QuadPart / ( double ) getScreenRefreshRate ( ) );
     }
 
     Int64 qpcounter ( ) const noexcept {
-
         LARGE_INTEGER c;
         QueryPerformanceCounter ( &c );
-
         return c.QuadPart;
     }
 
@@ -431,10 +365,18 @@ struct Pacer {
 
     void reset ( const Int64 frames_per_second_ ) noexcept;
 
-    Pacer ( ) noexcept : m_frequency ( qpfrequency ( ) ) { reset ( getScreenRefreshRate ( ) ); }
-    Pacer ( const Int32 frames_per_second_ ) noexcept : m_frequency ( qpfrequency ( ) ) { reset ( frames_per_second_ ); }
+    Pacer ( ) noexcept :
+        m_frequency ( qpfrequency ( ) ) {
+        reset ( getScreenRefreshRate ( ) );
+    }
+    Pacer ( const Int32 frames_per_second_ ) noexcept :
+        m_frequency ( qpfrequency ( ) ) {
+        reset ( frames_per_second_ );
+    }
 
-    ~Pacer ( ) { timeEndPeriod ( ); }
+    ~Pacer ( ) {
+        timeEndPeriod ( );
+    }
 
     HrTimePoint pace ( ) noexcept;
 
@@ -444,7 +386,6 @@ struct Pacer {
     IntDuration now ( ) noexcept;
 
     Int64 frequency ( ) const noexcept {
-
         return m_frequency;
     }
 };
@@ -501,7 +442,7 @@ LineSegment getBoundingBox ( const LineSegment & a_ ) noexcept {
 
 bool isPointOnLine ( const LineSegment & a_, const Point & b_ ) noexcept {
 
-    // Move the image, so that a_.first is on (0|0)...
+    // Move the image, so that a_.first is on (0|0).
 
     const float r = perpDotProduct<float> ( { a_ [ 1 ].x - a_ [ 0 ].x, a_ [ 1 ].y - a_ [ 0 ].y }, { b_.x - a_ [ 0 ].x, b_.y - a_ [ 0 ].y } );
 
@@ -510,7 +451,7 @@ bool isPointOnLine ( const LineSegment & a_, const Point & b_ ) noexcept {
 
 bool isPointRightOfLine ( const LineSegment & a_, const Point & b_ ) noexcept {
 
-    // Move the image, so that a_.first is on (0|0)...
+    // Move the image, so that a_.first is on (0|0).
 
     return perpDotProduct<float> ( { a_ [ 1 ].x - a_ [ 0 ].x, a_ [ 1 ].y - a_ [ 0 ].y }, { b_.x - a_ [ 0 ].x, b_.y - a_ [ 0 ].y } ) < 0.0f;
 }
